da6d64d94f4e6952313d7b54e3b905692670b709
chris pentecost <cjpentecost15@gmail.com>
2023-02-07 19:43:37 -0600

    finsihed q8

--- scripts/data_analyst_job_titles_cjpscript.sql
+++ scripts/data_analyst_job_titles_cjpscript.sql
@@ -45,8 +45,15 @@ ORDER BY avg_rating DESC;
 --NE
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs;
+--881
 
 -- 8.	How many unique job titles are there for California companies?
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE location='CA';
+--230
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
