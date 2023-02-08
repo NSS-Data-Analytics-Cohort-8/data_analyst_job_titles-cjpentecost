8349a7248b7009dcc291fd5f6ca75c19de1cb09c
chris pentecost <cjpentecost15@gmail.com>
2023-02-07 20:48:15 -0600

    finished q12

--- scripts/data_analyst_job_titles_cjpscript.sql
+++ scripts/data_analyst_job_titles_cjpscript.sql
@@ -70,8 +70,19 @@ ORDER BY avg_rating DESC;
 -- Unilever 4.4.1999998090000000
 
 -- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE title LIKE '% Analyst %';
+
+--382
 
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+
+SELECT title
+FROM data_analyst_jobs
+WHERE LOWER(title) NOT LIKE LOWER('%Analyst%') AND LOWER(title)NOT LIKE LOWER('%Analytics%');
+--4 , Tableau
+
 
 -- **BONUS:**
 -- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
